7037bc19d61a566f452c6d6fb2b67389265522af
kmkkiii <rskkmkmk@gmail.com>
2022-08-13 23:32:38 +0900

    implement api client

---
+++ src/services/users/get-user.ts
@@ -0,0 +1,26 @@
+import type { ApiContext, User } from "types";
+import { fetcher } from "utils";
+
+export type GetUserParams = {
+  /**
+   * ユーザーID
+   */
+  id: number;
+};
+
+const getUser = async (
+  context: ApiContext,
+  { id }: GetUserParams
+): Promise<User> => {
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, "")}/users/${id}`,
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+};
+
+export default getUser;
